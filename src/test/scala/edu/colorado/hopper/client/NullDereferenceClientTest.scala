cf53b71c66c72ee001e519c1f3d3b87b298bb9c4
Shawn Meier <shawn.meier@colorado.edu>
2019-11-26 14:31:40 -0700

    unit test for null deref

---
+++ src/test/scala/edu/colorado/hopper/client/NullDereferenceClientTest.scala
@@ -0,0 +1,20 @@
+package edu.colorado.hopper.client
+import java.io.File
+
+import edu.colorado.hopper.client.android.AndroidNullDereferenceClient
+import edu.colorado.hopper.client.android.AndroidNullDereferenceClientTests.printTestFailureMsg
+import edu.colorado.thresher.core.Options
+import edu.colorado.walautil.{LoopUtil, Timer}
+import org.scalatest.FunSuite
+
+import scala.sys.process.Process
+class NullDereferenceClientTest extends FunSuite {
+
+  test("Load Apk"){
+
+  }
+  test("AndroidNullDereferenceClient") {
+    //    val regressionDir = new File("src/test/java/nulls/")
+  }
+
+}
